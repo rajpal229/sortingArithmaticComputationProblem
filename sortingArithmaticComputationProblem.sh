9175bea103f5ec5e4d87e7d8724813894e803293
rajpal229 <rajpalrawat229@gmail.com>
2022-11-24 15:40:45 +0530

    Added a+b*c

--- sortingArithmaticComputationProblem.sh
+++ sortingArithmaticComputationProblem.sh
@@ -1,5 +1,10 @@
+echo "Welcome to the Sorting Arithmetic Computation Problem"
+
 read -p "Enter first value 'a' : " a;
 read -p "Enter second value 'b' : " b;
 read -p "Enter third value 'c' : " c;
 
 Echo "values are $a $b $c"
+
+compute1=$(($a+$b*$c))
+echo "a+b*c = "$compute1
