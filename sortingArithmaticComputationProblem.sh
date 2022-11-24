1929cabf8d3009a32bb8052629235647deb83e72
rajpal229 <rajpalrawat229@gmail.com>
2022-11-24 16:01:03 +0530

    Added a%b+c

--- sortingArithmaticComputationProblem.sh
+++ sortingArithmaticComputationProblem.sh
@@ -6,6 +6,14 @@ read -p "Enter third value 'c' : " c;
 
 Echo "values are $a $b $c"
 
+compute1=$(($a+$b*$c))
+echo "a+b*c = "$compute1
+
+compute2=$(($a*$b+$c))
+echo "a*b+c = "$compute2
+
 compute3=`awk 'BEGIN{printf("%0.2f", '$c'+'$a'/'$b')}'`
 echo "c+a/b = "$compute3
 
+compute2=$(($a%$b+$c))
+echo "a%b+c = "$compute2
