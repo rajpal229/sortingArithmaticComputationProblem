0f355cdb985096a6c4f2130f413c084e52dbb26d
rajpal229 <rajpalrawat229@gmail.com>
2022-11-24 16:32:04 +0530

    Store the results in a Dictionary

--- sortingArithmaticComputationProblem.sh
+++ sortingArithmaticComputationProblem.sh
@@ -7,4 +7,23 @@ read -p "Enter third value 'c' : " c;
 Echo "values are $a $b $c"
 
 compute1=$(($a+$b*$c))
-echo "a+b*c = "$compute1
+
+compute2=$(($a*$b+$c))
+
+compute3=`awk 'BEGIN{printf("%0.2f", '$c'+'$a'/'$b')}'`
+
+compute4=$(($a%$b+$c))
+
+
+declare -A compute
+
+compute[compute1]=$compute1
+compute[compute2]=$compute2
+compute[compute3]=$compute3
+compute[compute4]=$compute4
+
+echo "a+b*c = " ${compute[compute1]}
+echo "a*b+c = " ${compute[compute2]}
+echo "c+a/b = " ${compute[compute3]}
+echo "a%b+c = " ${compute[compute4]}
+echo "All values in Dictionary [ ${compute[@]} ]"
